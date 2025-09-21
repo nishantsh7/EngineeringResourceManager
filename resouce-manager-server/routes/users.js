8727df449656c893de6b6227a4f196c116e48f76
LoKe220723 <lokisharma101@gmail.com>
2025-09-22 02:04:35 +0530

    ..

--- resouce-manager-server/routes/users.js
+++ resouce-manager-server/routes/users.js
@@ -1,11 +1,12 @@
     const express = require('express');
     const router = express.Router();
-    const { updateUser } = require('../controllers/userController');
-    const { protect } = require('../middleware/authMiddleware');
-    const { updateUser, updateUserAdmin } = require('../controllers/userController');
+    const { updateUser,updateUserAdmin } = require('../controllers/userController');
+    const { protect, isManager} = require('../middleware/authMiddleware');
+    
+   
 
     // PUT /api/users/:id
-    // This route is protected, meaning a user must be logged in.
+    
     router.put('/admin/:id', protect, isManager, updateUserAdmin);
     router.put('/:id', protect, updateUser);
     module.exports = router;
